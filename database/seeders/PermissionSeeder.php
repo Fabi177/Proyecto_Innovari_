de9c0bde67575ad147ee0575d7127dc6dd875bf2
Fabi177 <genaro.camacho@educacionadventista.org.ar>
2025-11-09 22:38:03 -0300

    Update PermissionSeeder.php

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 
-class PermissionSeeder extends Seeder
+class RolePermissionSeeder extends Seeder
 {
     public function run(): void
     {
